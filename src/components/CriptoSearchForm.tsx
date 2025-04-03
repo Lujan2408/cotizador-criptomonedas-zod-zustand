8e8e8edece942f2a8b2c97b17924e8d77215261d
Jose David <lujan9495@gmail.com>
2025-04-03 15:59:54 -0500

    Typing with zod schema and showing currencies options

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,10 +1,17 @@
+import { currencies } from "../data"; 
+
 export default function CriptoSearchForm() {
   return (
     <form className="form">
       <div className="field">
         <label htmlFor="currency">Moneda:</label>
         <select name="currency" id="currency">
+
           <option value="">-- Seleccione --</option>
+          {currencies.map(currency => (
+            <option value="">{currency.name}</option>
+          ))}
+          
         </select>
       </div>
 
---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+import { Currency } from "../types";
+
+export const currencies : Currency[] = [
+  { code: 'COP', name: 'Pesos Colombianos'},
+  { code: 'USD', name: 'Dolar de Estados Unidos'},
+  { code: 'EUR', name: 'Euro'},
+  { code: 'MXN', name: 'Peso Mexicano'},
+  { code: 'GBP', name: 'Libra Esterlina'}
+]
