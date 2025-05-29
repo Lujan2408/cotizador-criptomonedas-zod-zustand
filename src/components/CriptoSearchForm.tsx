9bb1dccefe84a60be46bb41064e90d90aaf78f4e
Jose David <lujan9495@gmail.com>
2025-05-28 22:11:01 -0500

    Adding form validation to check if there are empty inputs

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { currencies } from "../data"; 
 import { useCryptoStore } from "../store";
 import { pair } from "../types";
+import ErrorMessage from "./ErrorMessage";
 
 export default function CriptoSearchForm() {
 
@@ -12,6 +13,7 @@ export default function CriptoSearchForm() {
     criptoCurrency: ''
   })
 
+  const [error, setError] = useState('')
   const handleChange = (e: ChangeEvent<HTMLSelectElement> ) => {
     setPair({
       ...pair,
@@ -19,41 +21,59 @@ export default function CriptoSearchForm() {
     })
   }
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // "pair" es un objeto, con el Object.values lo convierte a un array y con includes revisamos si tiene strings vacíos 
+    if(Object.values(pair).includes('')) {
+      setError('Todos los Campos son Obligatorios')
+      return
+    }
+    setError('')
+  }
+
   return (
-    <form className="form">
-      <div className="field">
-        <label htmlFor="currency">Moneda:</label>
-        <select 
-          name="currency" 
-          id="currency"
-          onChange={handleChange}
+      <form 
+        className="form"
+        onSubmit={handleSubmit}
         >
-          <option value="">-- Seleccione --</option>
-          {currencies.map((currency) => (
-            <option key={currency.code} value={currency.code}>
-              {currency.name}
-            </option>
-          ))}
-        </select>
-      </div>
 
-      <div className="field">
-        <label htmlFor="criptocurrency">Criptomoneda:</label>
-        <select 
-          name="criptoCurrency" 
-          id="criptocurrency"
-          onChange={handleChange} 
-        >
-          <option value="">-- Seleccione --</option>
-          {cryptocurrencies.map((crypto) => (
-            <option key={crypto.CoinInfo.FullName} value={crypto.CoinInfo.Name}>
-              {crypto.CoinInfo.FullName}
-            </option>
-          ))}
-        </select>
-      </div>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+        <div className="field">
+          <label htmlFor="currency">Moneda:</label>
+          <select 
+            name="currency" 
+            id="currency"
+            value={pair.currency}
+            onChange={handleChange}
+          >
+            <option value="">-- Seleccione --</option>
+            {currencies.map((currency) => (
+              <option key={currency.code} value={currency.code}>
+                {currency.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <input type="submit" value="Cotizar" />
-    </form>
+        <div className="field">
+          <label htmlFor="criptocurrency">Criptomoneda:</label>
+          <select 
+            name="criptoCurrency" 
+            id="criptocurrency"
+            value={pair.criptoCurrency}
+            onChange={handleChange} 
+          >
+            <option value="">-- Seleccione --</option>
+            {cryptocurrencies.map((crypto) => (
+              <option key={crypto.CoinInfo.FullName} value={crypto.CoinInfo.Name}>
+                {crypto.CoinInfo.FullName}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <input type="submit" value="Cotizar" />
+      </form>
   );
 }
