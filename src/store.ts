7f31f197ac4631c7f0c7e105a1fe335697bac788
Jose David <lujan9495@gmail.com>
2025-05-29 08:52:30 -0500

    Putting API response in the state

--- src/store.ts
+++ src/store.ts
@@ -1,26 +1,42 @@
 import { create } from "zustand";
+import { z } from "zod" 
 import {devtools} from "zustand/middleware"
-import { cryptoCurrency, pair } from "./types";
+import { cryptoCurrency, cryptoPrice, pair } from "./types";
 import { getCryptos, fetchCurrentCryptoPrice } from "./services/CryptoService";
 
 
 type CryptoStore = {
   cryptoCurrencies: cryptoCurrency[],
+  result: cryptoPrice
   fetchCryptos: () => Promise<void>,
   fetchData: (pair : pair) => Promise<void>
 }
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
-  cryptoCurrencies: [], // State
+    // State
+    cryptoCurrencies: [],
+    result: {
+      IMAGEURL: z.string(),
+      PRICE: z.string(),
+      HIGHDAY: z.string(),
+      LOWDAY: z.string(),
+      CHANGEPCT24HOUR: z.string(),
+      LASTUPDATE: z.string(),
+    },
 
-  fetchCryptos: async () => { // Mandamos a llamar la función cuando la app esté lista
-    const cryptoCurrencies = await getCryptos()
-    set(() => ({
-      cryptoCurrencies
-    }))
-  },
-  fetchData: async (pair) => {
-   const result = await fetchCurrentCryptoPrice(pair)
-   console.log(result)
-  }
-})))
+    fetchCryptos: async () => {
+      // Mandamos a llamar la función cuando la app esté lista
+      const cryptoCurrencies = await getCryptos();
+      set(() => ({
+        cryptoCurrencies,
+      }));
+    },
+    fetchData: async (pair) => {
+      const result = await fetchCurrentCryptoPrice(pair);
+
+      set(() => ({
+        result
+      }))
+    },
+  }))
+);
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
-import { CurrencySchema, cryptoCurrencyResponseSchema, pairSchema } from "../schema/crypto-schema";
+import { CurrencySchema, cryptoCurrencyResponseSchema, pairSchema, cryptoPriceSchema } from "../schema/crypto-schema";
 
 export type Currency = z.infer<typeof CurrencySchema>
 export type cryptoCurrency = z.infer<typeof cryptoCurrencyResponseSchema>
 export type pair = z.infer<typeof pairSchema>
+export type cryptoPrice = z.infer<typeof cryptoPriceSchema>
